e08ec8aa07fb1b6d7b3388371380b5ea8ef3e429
aiba4531 <aiba4531@colorado.edu>
2024-06-17 10:09:10 -0600

    Stashing local changes

--- phase1/main.py
+++ phase1/main.py
@@ -13,106 +13,106 @@ if __name__ == "__main__":
 
 ### DEFINE THE SATELLITE OBJECTS:
     # Define a sensor model:
-    sens1 = sensor(name = 'Sensor 1', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-    sens2 = sensor(name = 'Sensor 2', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+#     sens1 = sensor(name = 'Sensor 1', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+#     sens2 = sensor(name = 'Sensor 2', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
 
-    # Define targets for the satellites to track:
-    # TODO: should we just make the satellite track any target it can see?
-    targetIDs = [1,2]
-
-    # Define local estimators:
-    local1 = localEstimator(targetIDs = targetIDs)
-    local2 = localEstimator(targetIDs = targetIDs)
-    central = centralEstimator(targetIDs = targetIDs) # TODO: why not just make centralized always do all targets? since it is the baseline?
-
-    # Define the satellites:
-    sat1 = satellite(name = 'Sat1', sensor = sens1, targetIDs=targetIDs, estimator = local1, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = 50, nu = 0, color='b')
-    sat2 = satellite(name = 'Sat2', sensor = sens2, targetIDs=targetIDs, estimator = local2, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = 50, nu = 0, color='r')
-    
-    sats = [sat1]#, sat2]
-
-# DEFINE THE TARGET OBJECTS:
-    targ1 = boat_target(name = 'Targ1', targetID=1, r = np.array([6378, 0, 0, 0, 0, 0]),color = 'k')
-    targ2 = HGV_target(name = 'Targ2', targetID=2, X = [3, 0.1, 0, 6378, 0, 0.1], color = 'r')
-     
-    targs = [targ1, targ2]
-
-# Define the communication network:
-    commNet = comms(sats, range = 5000 * u.km, displayStruct = True)
-
-# Create an environment instance:
-    env = environment(sats, targs, commNet, central)
-
-# Simulate the satellites through a vector of time:
-    time_vec = np.linspace(0, 20, 21) * u.minute
-    env.simulate(time_vec, display = True)
-
-# Save the gif:
-    env.render_gif(fileName = 'satellite_simulation.gif', fps = 5)
-
-
-# ### CLUSTER SIM BELOW:
-
-# # DEFINE THE SATELLITE OBJECTS:
-
-#     # Define a sensor model:
-#     sens1_1 = sensor(name = 'Sensor 1.1', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-#     sens1_2 = sensor(name = 'Sensor 1.2', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-#     sens1_3 = sensor(name = 'Sensor 1.3', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-#     sens1_4 = sensor(name = 'Sensor 1.4', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-
-#     sens2_1 = sensor(name = 'Sensor 2.1', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-#     sens2_2 = sensor(name = 'Sensor 2.2', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-#     sens2_3 = sensor(name = 'Sensor 2.3', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-#     sens2_4 = sensor(name = 'Sensor 2.4', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
-    
 #     # Define targets for the satellites to track:
 #     # TODO: should we just make the satellite track any target it can see?
-#     targetIDs = [1]
-
+#     targetIDs = [1,2]
 
 #     # Define local estimators:
-#     local1_1 = localEstimator(targetIDs = targetIDs)
-#     local1_2 = localEstimator(targetIDs = targetIDs)
-#     local1_3 = localEstimator(targetIDs = targetIDs)
-#     local1_4 = localEstimator(targetIDs = targetIDs)
-    
-#     local2_1 = localEstimator(targetIDs = targetIDs)
-#     local2_2 = localEstimator(targetIDs = targetIDs)
-#     local2_3 = localEstimator(targetIDs = targetIDs)
-#     local2_4 = localEstimator(targetIDs = targetIDs)
+#     local1 = localEstimator(targetIDs = targetIDs)
+#     local2 = localEstimator(targetIDs = targetIDs)
+#     central = centralEstimator(targetIDs = targetIDs) # TODO: why not just make centralized always do all targets? since it is the baseline?
 
-#     # Central Estimator:
-#     central = centralEstimator(targetIDs = targetIDs) 
+#     # Define the satellites:
+#     sat1 = satellite(name = 'Sat1', sensor = sens1, targetIDs=targetIDs, estimator = local1, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = 50, nu = 0, color='b')
+#     sat2 = satellite(name = 'Sat2', sensor = sens2, targetIDs=targetIDs, estimator = local2, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = 50, nu = 0, color='r')
     
-#     ## Cluster 1: Polar orbits:
-#     sat1_1 = satellite(name = 'Sat1', sensor = sens1_1, targetIDs=targetIDs, estimator = local1_1, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -90, nu = 0, color='b')
-#     sat1_2 = satellite(name = 'Sat2', sensor = sens1_2, targetIDs=targetIDs, estimator = local1_2, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -70, nu = 0, color='b')
-#     sat1_3 = satellite(name = 'Sat3', sensor = sens1_3, targetIDs=targetIDs, estimator = local1_3, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -50, nu = 0, color='b')
-#     sat1_4 = satellite(name = 'Sat4', sensor = sens1_4, targetIDs=targetIDs, estimator = local1_4, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -30, nu = 0, color='b')
-
-#     ## Cluster 2: Equatorial orbits:
-#     sat2_1 = satellite(name = 'Sat5', sensor = sens2_1, targetIDs=targetIDs, estimator = local2_1, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -180, nu = 0, color='r')
-#     sat2_2 = satellite(name = 'Sat6', sensor = sens2_2, targetIDs=targetIDs, estimator = local2_2, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -160, nu = 0, color='r')
-#     sat2_3 = satellite(name = 'Sat7', sensor = sens2_3, targetIDs=targetIDs, estimator = local2_3, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -140, nu = 0, color='r')
-#     sat2_4 = satellite(name = 'Sat8', sensor = sens2_4, targetIDs=targetIDs, estimator = local2_4, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -120, nu = 0, color='r')
-    
-#     sats = [sat1_1, sat1_2, sat1_3, sat1_4, sat2_1, sat2_2, sat2_3, sat2_4]
+#     sats = [sat1]#, sat2]
 
 # # DEFINE THE TARGET OBJECTS:
-#     targ1 = target(name = 'Targ1', targetID=1, r = np.array([6378, 0, 0, 0, 0, 0]),color = 'k')
+#     targ1 = boat_target(name = 'Targ1', targetID=1, r = np.array([6378, 0, 0, 0, 0, 0]),color = 'k')
+#     targ2 = HGV_target(name = 'Targ2', targetID=2, X = [3, 0.1, 0, 6378, 0, 0.1], color = 'r')
      
-#     targs = [targ1]
+#     targs = [targ1, targ2]
 
 # # Define the communication network:
-#     comms = comms(sats, range = 5000 * u.km, displayStruct = True)
+#     commNet = comms(sats, range = 5000 * u.km, displayStruct = True)
 
 # # Create an environment instance:
-#     env = environment(sats, targs, comms, central)
+#     env = environment(sats, targs, commNet, central)
 
 # # Simulate the satellites through a vector of time:
-#     time_vec = np.linspace(0, 50, 51) * u.minute
+#     time_vec = np.linspace(0, 20, 21) * u.minute
 #     env.simulate(time_vec, display = True)
 
 # # Save the gif:
 #     env.render_gif(fileName = 'satellite_simulation.gif', fps = 5)
+
+
+### CLUSTER SIM BELOW:
+
+# DEFINE THE SATELLITE OBJECTS:
+
+    # Define a sensor model:
+    sens1_1 = sensor(name = 'Sensor 1.1', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    sens1_2 = sensor(name = 'Sensor 1.2', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    sens1_3 = sensor(name = 'Sensor 1.3', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    sens1_4 = sensor(name = 'Sensor 1.4', fov = 115, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+
+    sens2_1 = sensor(name = 'Sensor 2.1', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    sens2_2 = sensor(name = 'Sensor 2.2', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    sens2_3 = sensor(name = 'Sensor 2.3', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    sens2_4 = sensor(name = 'Sensor 2.4', fov = 100, sensorError = np.array([1, 1]), detectError= 0.05, resolution = 720)
+    
+    # Define targets for the satellites to track:
+    # TODO: should we just make the satellite track any target it can see?
+    targetIDs = [1]
+
+
+    # Define local estimators:
+    local1_1 = localEstimator(targetIDs = targetIDs)
+    local1_2 = localEstimator(targetIDs = targetIDs)
+    local1_3 = localEstimator(targetIDs = targetIDs)
+    local1_4 = localEstimator(targetIDs = targetIDs)
+    
+    local2_1 = localEstimator(targetIDs = targetIDs)
+    local2_2 = localEstimator(targetIDs = targetIDs)
+    local2_3 = localEstimator(targetIDs = targetIDs)
+    local2_4 = localEstimator(targetIDs = targetIDs)
+
+    # Central Estimator:
+    central = centralEstimator(targetIDs = targetIDs) 
+    
+    ## Cluster 1: Polar orbits:
+    sat1_1 = satellite(name = 'Sat1', sensor = sens1_1, targetIDs=targetIDs, estimator = local1_1, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -90, nu = 0, color='b')
+    sat1_2 = satellite(name = 'Sat2', sensor = sens1_2, targetIDs=targetIDs, estimator = local1_2, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -70, nu = 0, color='b')
+    sat1_3 = satellite(name = 'Sat3', sensor = sens1_3, targetIDs=targetIDs, estimator = local1_3, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -50, nu = 0, color='b')
+    sat1_4 = satellite(name = 'Sat4', sensor = sens1_4, targetIDs=targetIDs, estimator = local1_4, a = Earth.R + 1000 * u.km, ecc = 0, inc = 90, raan = -45, argp = -30, nu = 0, color='b')
+
+    ## Cluster 2: Equatorial orbits:
+    sat2_1 = satellite(name = 'Sat5', sensor = sens2_1, targetIDs=targetIDs, estimator = local2_1, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -180, nu = 0, color='r')
+    sat2_2 = satellite(name = 'Sat6', sensor = sens2_2, targetIDs=targetIDs, estimator = local2_2, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -160, nu = 0, color='r')
+    sat2_3 = satellite(name = 'Sat7', sensor = sens2_3, targetIDs=targetIDs, estimator = local2_3, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -140, nu = 0, color='r')
+    sat2_4 = satellite(name = 'Sat8', sensor = sens2_4, targetIDs=targetIDs, estimator = local2_4, a = Earth.R + 1000 * u.km, ecc = 0, inc = 0, raan = 0, argp = -120, nu = 0, color='r')
+    
+    sats = [sat1_1, sat1_2, sat1_3, sat1_4, sat2_1, sat2_2, sat2_3, sat2_4]
+
+# DEFINE THE TARGET OBJECTS:
+    targ1 = boat_target(name = 'Targ1', targetID=1, r = np.array([6378, 0, 0, 0, 0, 0]),color = 'k')
+     
+    targs = [targ1]
+
+# Define the communication network:
+    comms = comms(sats, range = 5000 * u.km, displayStruct = True)
+
+# Create an environment instance:
+    env = environment(sats, targs, comms, central)
+
+# Simulate the satellites through a vector of time:
+    time_vec = np.linspace(0, 50, 51) * u.minute
+    env.simulate(time_vec, display = True)
+
+# Save the gif:
+    env.render_gif(fileName = 'satellite_simulation.gif', fps = 5)
