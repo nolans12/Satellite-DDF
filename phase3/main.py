bbcc7dc59beed131ee9bad7e681453f9e167ab6e
Ryan Draves <dravesr@umich.edu>
2024-09-20 18:57:34 -0600

    fix

--- phase3/main.py
+++ phase3/main.py
@@ -282,7 +282,7 @@ def main(
     )
 
     # Save gifs:
-    env.render_gifs(plot_config=cfg.plot, save_name=cfg.output_prefix)
+    env.render_gifs(plot_config=cfg.plot, save_name=cfg.plot.output_prefix)
 
 
 if __name__ == '__main__':
